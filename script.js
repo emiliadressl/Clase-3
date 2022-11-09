79b0bc2bba86eead9a2b84b56f411df07dcb1781
emiliadressl <117779750+emiliadressl@users.noreply.github.com>
2022-11-08 21:12:34 -0300

    carga de archivos

---
+++ script.js
@@ -0,0 +1,34 @@
+let firstName = "Emilia";
+console.log(firstName)
+let lastName;
+console.log(lastName);
+let score1 = 3;
+let score2 = 6;
+let average = (score1+score2)/2;
+console.log(`you scored ${average}`);
+if (score1 === 3) {
+    document.write("congrats, it's a three!")
+}
+if (!lastName){
+    document.write("enter last name")
+}
+
+
+
+/* necesita de un navegador que lo lea/runee, tonces necesita tmb un archivo html
+//comentario de una sola linea 
+tipos de datos
+    string palabras, entre comillas (dobles o simples) (entre tilde inverso te deja hacer salto de linea dentro del string)
+    numbers
+    boolean t/f
+    undefined falta la info
+let abre variable, alfanumericas, empiezan siempre en minusc nunca numero. puede tener _, no -. no espacios, se marca con mayusc
+en front console.log es solo p una, a la hora de subir la pagina hay que borrar todos. en back se puede guardar en .txt p revisar en caso de errores
+line 8 concatena string. podria poner string + V, poniendo el string con tilde invert dentro pongo V tipo ${V} y le pone el espacio de una, no necesito agregarlo en el string anterior
+= define variable/obj
+=== significa equivalente (o =)
+if no lleva ;
+line 9 es un boolean(un if)
+line 10 instrucciones en base a t/f
+! equals not, va adelante de la variable
+*/
